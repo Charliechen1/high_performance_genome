de73b4cd9b76671f77c00ad5ddefa3a580fe9aef
Jialic Chen <j_chen@cori19.nersc.gov>
2020-01-25 23:32:06 -0800

    [20200125] Switch back to cpu version, added a run.sh file

--- src/models/LSTM/run.sh
+++ src/models/LSTM/run.sh
@@ -1,1 +1,11 @@
-nohup /usr/common/software/pytorch/v1.2.0/bin/python train.py -s 100 -e 30 -H 20 -b 64 -p 300 -c "../../../config/main.conf" -d >> log.txt&
+if [[ $1 == "--gpu" ]] || [[ $1 == "-g" ]]
+then
+    # gpu version execution
+    nohup /usr/common/software/pytorch/v1.2.0-gpu/bin/python train.py -s 1 -e 30 -H 20 -b 64 -p 300 -c -g "../../../config/main.conf" -d > log_`date +"%m-%d-%Y"`.txt &
+elif [[ $1 == "--cpu" ]] || [[ $1 == "-c" ]]
+then
+    # cpu version execution
+    nohup /usr/common/software/pytorch/v1.2.0/bin/python train.py -s 1 -e 30 -H 20 -b 64 -p 300 -c "../../../config/main.conf" -d > log_`date +"%m-%d-%Y"`.txt &
+else
+    echo "unknown command"
+fi
