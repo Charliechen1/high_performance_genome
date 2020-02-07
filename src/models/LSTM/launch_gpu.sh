b36dd194761261fa570b89f22c0c28a039dcced5
Jude Roque Fernandes <judef@cori14.nersc.gov>
2020-02-06 16:05:37 -0800

    created launching and running scripts to run train script on gpu

--- src/models/LSTM/launch_gpu.sh
+++ src/models/LSTM/launch_gpu.sh
@@ -1,2 +1,24 @@
+CONF="hpg_gpu_env.yml"
+
+#Check that config file exists and grab environment name
+if [ -e $CONF ]
+then
+    ENV=$(head -n 1 $CONF | cut -f2 -d ' ')
+else
+    echo "Environment config not found"
+    exit 1
+fi
+
+#Check if environment is already installed and install if needed
+EXIST=`conda info --envs | grep $ENV | wc -l`
+if [ $EXIST == "1" ]
+then
+    echo "Environment already installed"
+else
+    echo "Installing environment"
+    conda env create -f $CONF
+fi
+
+echo "Launching GPU interactive session"
 module load esslurm
 salloc -C gpu -N 1 -t 60 -c 10 --gres=gpu:1
--- src/models/LSTM/run_gpu.sh
+++ src/models/LSTM/run_gpu.sh
@@ -1,20 +1,3 @@
-#module load esslurm
-#salloc -C gpu -N 1 -t 60 -c 10 --gres=gpu:1
-CONF="hpg_gpu_env.yml"
-if [ -e $CONF ]; then
-    #echo "yml file found"
-    ENV=$(head -n 1 $CONF | cut -f2 -d ' ')
-    # Check if you are already in the environment
-    if [[ $PATH != *$ENV* ]]; then
-        # check whether the environment has been installed
-        $EXIST=`conda info --envs | grep $ENV | wc -l`
-        if [ $EXIST != "1" ]; then
-            conda env create -f $CONF
-        fi
-    fi
-    source activate $ENV
-fi
-
-module load python cuda
-module load pytorch/v1.1.0-gpu
-srun -n 1 nohup python train.py -s 1 -e 30 -H 20 -b 64 -p 300 -c "../../../config/main.conf" -g -d > log_`date +"%m-%d-%Y"`.txt
+#module load python cuda
+#module load pytorch/v1.1.0-gpu
+srun -n 1 nohup ~/.conda/envs/hpg_gpu_env/bin/python train.py -s 1 -e 30 -H 20 -b 64 -p 300 -c "../../../config/main.conf" -g -d > log_`date +"%m-%d-%Y"`.txt
