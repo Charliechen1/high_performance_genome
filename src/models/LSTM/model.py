6e4bf741d25cf611fb4963be061a92d24ca2f5c5
Charliechen1 <Charliechen_1@163.com>
2020-02-16 17:51:15 -0800

    [Charlie 02162020] added two affine layer to the LSTM model. Refact the code to make the APIs reusable and scalable.

--- src/models/LSTM/model.py
+++ src/models/LSTM/model.py
@@ -4,26 +4,29 @@ import torch.nn.functional as F
 
 class LSTMTagger(nn.Module):
 
-    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size, batch_size, padding_idx=0):
+    def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size, padding_idx=0):
         super(LSTMTagger, self).__init__()
         self.hidden_dim = hidden_dim
 
         self.word_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_idx)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True, bidirectional=True)
-        self.hidden2tag = nn.Linear(hidden_dim * 2, tagset_size)
-        self.dropout_layer = nn.Dropout(p=0.2)
-        self.batch_size = batch_size
+        self.hid2hid1 = nn.Linear(hidden_dim * 2, hidden_dim * 2)
+        self.hid2hid2 = nn.Linear(hidden_dim * 2, hidden_dim)
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+        self.dropout_layer = nn.Dropout(p=0.75)
         self.padding_idx = padding_idx
         self.softmax = nn.LogSoftmax()
     
     def forward(self, X):
-        batch_size, seq_len = X.size()
+        X_size, seq_len = X.size()
         embeds = self.word_embeddings(X)
-        lstm_out, _ = self.lstm(embeds.view(batch_size, seq_len, -1))
-        lstm_out = lstm_out.view(batch_size, seq_len, -1)
+        lstm_out, _ = self.lstm(embeds.view(X_size, seq_len, -1))
+        lstm_out = lstm_out.view(X_size, seq_len, -1)
         # current we just take the last hidden state of the LSTM, later will modify to attention layer
         # we do not want to take the state for padding
         last_state = lstm_out.mean(1)
-        tag_space = self.hidden2tag(last_state)
+        hidden_res = F.relu(self.hid2hid1(last_state))
+        hidden_res = F.relu(self.hid2hid2(hidden_res))
+        tag_space = self.hidden2tag(hidden_res)
         tag_scores = F.log_softmax(tag_space, dim=1)
         return tag_scores
--- src/models/LSTM/run_gpu.sh
+++ src/models/LSTM/run_gpu.sh
@@ -1,3 +1,8 @@
 #module load python cuda
 #module load pytorch/v1.1.0-gpu
-srun -n 1 nohup ~/.conda/envs/hpg_gpu_env/bin/python train.py -s 1 -e 30 -H 20 -b 64 -p 300 -c "../../../config/main.conf" -g -d > log_`date +"%m-%d-%Y"`.txt
+if [[ ! -d "log" ]]
+then
+    mkdir log
+fi
+
+nohup srun -n 1 ~/.conda/envs/hpg_gpu_env/bin/python train.py -s 1 -e 80 -E 8 -H 80 -f 10 -p 500 -P 200 -c "../../../config/main.conf" -g -d >> log/log_`date +"%m-%d-%Y"`.txt &
