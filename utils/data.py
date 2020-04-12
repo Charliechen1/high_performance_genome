03a316a8adc1e532e248fe19f34a615d0c31fe05
Charliechen1 <Charliechen_1@163.com>
2020-04-11 17:25:44 -0700

    [Charlie 03312020] Merged Nick's GCN model and fixed a small problem at model.conf

--- utils/data.py
+++ utils/data.py
@@ -56,14 +56,19 @@ def load_data(conf, logger, g_pool):
     g_pool['vocab'] = vocab
     fams = np.array(train["family_id"].value_counts().index)[::sample_every]
     g_pool['fams'] = fams
-    partition = train[train["family_id"].isin(fams)]
+    train_partition = train[train["family_id"].isin(fams)]
+    dev_partition = dev[dev["family_id"].isin(fams)]
     max_len = int(model_conf['Preprocess']['MaxLen'])
-    X = partition['aligned_sequence'].values
-    y = partition['family_id'].values
-    X_train_raw, X_test_raw, y_train_raw, y_test_raw = train_test_split(X, y, test_size=test_size, random_state=45)
+    
+    X_train = train_partition['aligned_sequence'].values
+    y_train_raw = train_partition['family_id'].values
+    X_test = dev_partition['aligned_sequence'].values
+    y_test_raw = dev_partition['family_id'].values
+    
+    #X_train_raw, X_test_raw, y_train_raw, y_test_raw = train_test_split(X, y, test_size=test_size, random_state=45)
     fam_vocab = {fam: idx for idx, fam in enumerate(fams)}
     g_pool['fam_vocab'] = fam_vocab
     
     y_train = np.array([fam_vocab[y] for y in y_train_raw])
     y_test = np.array([fam_vocab[y] for y in y_test_raw])
-    return X_train_raw, X_test_raw, y_train, y_test
+    return X_train, X_test, y_train, y_test
