9183aa533fe2af7f4a9a78ce983221437f49b373
Jude Fernandes <juderoque@gmail.com>
2020-04-20 21:14:12 -0700

    created batch scripts for contact map

--- src/contact_map/run.sh
+++ src/contact_map/run.sh
@@ -1,1 +1,22 @@
-nohup /global/homes/n/nswenson/.conda/envs/biopy/bin/python contactmap.py > log_`date +"%m-%d-%Y"`.txt&
+CONF="contact_map_env.yml"
+
+#Check that config file exists and grab environment name
+if [ -e $CONF ]
+then
+    ENV=$(head -n 1 $CONF | cut -f2 -d ' ')
+else
+    echo "Environment config not found"
+    exit 1
+fi
+
+#Check if environment is already installed and install if needed
+EXIST=`conda info --envs | grep $ENV | wc -l`
+if [ $EXIST == "1" ]
+then
+    echo "Environment already installed"
+else
+    echo "Installing environment"
+    conda env create -f $CONF
+fi
+
+sbatch batch_cpu.sh
