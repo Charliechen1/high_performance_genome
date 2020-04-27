43d6b6997bec2c3c4b50da9ee4db207cbc011385
swenson-nick <swenson.nicolas@gmail.com>
2020-04-26 17:17:40 -0700

    code clean up

--- src/models/GCN/model.py
+++ src/models/GCN/model.py
@@ -5,7 +5,8 @@ from torch_geometric.nn import GCNConv, global_add_pool as gap
 class GCN(torch.nn.Module):
     def __init__(self, target_dim, lm_embedding):
         super(GCN, self).__init__()
-        self.embedding = torch.nn.Embedding.from_pretrained(lm_embedding)
+        self.embedding = torch.nn.Embedding.from_pretrained(lm_embedding, freeze = True)
+        #self.embedding = torch.nn.Embedding(27, 80)
         self.conv1 = GCNConv(80, 128)
         self.conv2 = GCNConv(128, 128)
         self.conv3 = GCNConv(128, 256)
@@ -15,7 +16,7 @@ class GCN(torch.nn.Module):
     def forward(self, data):
         
         x, edge_index, batch = data.x, data.edge_index, data.batch
-                
+                                
         x = self.embedding(x.long())
         x = self.conv1(x, edge_index)
         x = F.relu(x)
@@ -26,6 +27,29 @@ class GCN(torch.nn.Module):
         x = self.conv3(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
+        x = gap(x, batch)
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.linear2(x)
+        x = torch.sigmoid(x)
+        
+        return x
+    
+class Toy(torch.nn.Module):
+    
+    def __init__(self, target_dim, lm_embedding):
+        
+        super(Toy, self).__init__()
+        self.embedding = torch.nn.Embedding.from_pretrained(lm_embedding, freeze = True)
+        self.linear1 = torch.nn.Linear(80, 128)
+        self.linear2 = torch.nn.Linear(128, target_dim)
+        
+    def forward(self, data):
+        
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        
+        x = self.embedding(x.long())
         x = gap(x, batch)
         x = self.linear1(x)
         x = F.relu(x)
--- src/models/GCN/train.py
+++ src/models/GCN/train.py
@@ -4,16 +4,23 @@ from torch_geometric.utils import from_scipy_sparse_matrix
 from torch_geometric.data import DataLoader
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from model import GCN
+from model import GCN, Toy
 import pickle
 import configparser
 import logging
 import sys
 import os
 import json
-import data
+import pdb_data
 import matplotlib.pyplot as plt
 from sklearn.metrics import auc
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+import numpy as np
+import seaborn as sns
+
+sys.path.append("../../../utils/")
+from data import prepare_sequence
 
 # global device param
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -24,72 +31,48 @@ def split_data(data_dict, aa_vocab, go_type = 'mf'):
     if go_type == 'mf':
         X = data_dict['X_mf'] 
         y = data_dict['y_mf']  
-        vocab = data_dict['mf_vocab']
+        go_vocab = data_dict['mf_vocab']
         
     if go_type == 'bp':
         X = data_dict['X_bp'] 
         y = data_dict['y_bp']  
-        vocab = data_dict['bp_vocab']
+        go_vocab = data_dict['bp_vocab']
         
     if go_type == 'cc':
         X = data_dict['X_cc'] 
         y = data_dict['y_cc']  
-        vocab = data_dict['cc_vocab']
+        go_vocab = data_dict['cc_vocab']
         
     X_train_temp, X_test, y_train_temp, y_test = train_test_split(X, y, test_size=0.125, random_state=41)
     X_train, X_val, y_train, y_val = train_test_split(X_train_temp, y_train_temp, test_size=0.125, random_state=41)
-    dataset_train = []
-    for data, raw_labels in zip(X_train, y_train):
+    
+    dataset_train = prepare_dataset(X_train, y_train, go_vocab, aa_vocab)    
+    dataset_val = prepare_dataset(X_val, y_val, go_vocab, aa_vocab)
+    dataset_test = prepare_dataset(X_test, y_test, go_vocab, aa_vocab)    
+        
+    return dataset_train, dataset_val, dataset_test, len(go_vocab)
+
+def prepare_dataset(X_data, y_data, go_vocab, aa_vocab):
+    
+    dataset = []
+    
+    for data, raw_labels in zip(X_data, y_data):
         x = prepare_sequence(data[0], aa_vocab, 3000).float()
         edge_index = from_scipy_sparse_matrix(data[1])[0]
-        labels = torch.tensor([vocab[y] for y in raw_labels])
-        targets = torch.zeros((len(vocab)))
+        labels = torch.tensor([go_vocab[y] for y in raw_labels])
+        targets = torch.zeros((len(go_vocab)))
         targets[labels] = 1
         targets = torch.tensor(targets)
-        dataset_train.append(Data(x = x, edge_index = edge_index, y = targets))
+        dataset.append(Data(x = x, edge_index = edge_index, y = targets))
         
-    dataset_val = []
-    for data, raw_labels in zip(X_val, y_val):
-        x = prepare_sequence(data[0], aa_vocab, 3000).float()
-        edge_index = from_scipy_sparse_matrix(data[1])[0]
-        labels = torch.tensor([vocab[y] for y in raw_labels])
-        targets = torch.zeros((len(vocab)))
-        targets[labels] = 1
-        targets = torch.tensor(targets)
-        dataset_val.append(Data(x = x, edge_index = edge_index, y = targets))
-        
-    dataset_test = []
-    for data, raw_labels in zip(X_test, y_test):
-        x = prepare_sequence(data[0], aa_vocab, 3000).float()
-        edge_index = from_scipy_sparse_matrix(data[1])[0]
-        labels = torch.tensor([vocab[y] for y in raw_labels])
-        targets = torch.zeros((len(vocab)))
-        targets[labels] = 1
-        targets = torch.tensor(targets)
-        dataset_val.append(Data(x = x, edge_index = edge_index, y = targets))
-        
-        
-    return dataset_train, dataset_val, dataset_test, len(vocab)
+    return dataset
 
-def prepare_sequence(seq, vocab, padding):
-    """
-    function to process the data, padding them
-    TODO later will move to specific preprocessing part
-    """
-    res = ['<PAD>'] * padding
-    res[:min(padding, len(seq))] = seq[:min(padding, len(seq))]
-    # use 0 for padding
-    idxs = [vocab[w] for w in res]
-    return torch.tensor(idxs, dtype=torch.long)
-
-
-def train(logger, model, dataset_train, dataset_val, target_dim, lr=0.00005, num_epoch = 50):
+def train(logger, model, dataset_train, dataset_val, target_dim, lr=0.00005, num_epoch = 100):
     
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=5e-4)
     criterion = torch.nn.BCELoss()
     train_loader = DataLoader(dataset_train, shuffle = True, batch_size=64)
     model.train()
-    logger.debug('')
     for epoch in range(num_epoch):
         for iteration, data in enumerate(train_loader):
             optimizer.zero_grad()
@@ -98,82 +81,53 @@ def train(logger, model, dataset_train, dataset_val, target_dim, lr=0.00005, num
             loss = criterion(out, data.y.float().reshape(-1, target_dim))
             loss.backward()
             optimizer.step()
-            if iteration % 40 == 0:
-                logger.debug(f'epoch {epoch + 1}, iteration {iteration}')
-                logger.debug(f'loss = {loss}')
-                pred = (out > 0.5).float()
-                label = data.y.float().reshape(-1, target_dim)
-                logger.debug(f'precision = {((label * pred).sum()/label.sum()).item()}')
-                logger.debug(f'recall = {((label * pred).sum()/pred.sum()).item()}')
-                logger.debug(f'total # of go terms = {label.sum().item()}')
-                logger.debug(f'predicted # of go terms = {pred.sum().item()}')
-                logger.debug('')
+            #if iteration % 40 == 0:
+            #    logger.debug(f'epoch {epoch + 1}, iteration {iteration}')
+            #    logger.debug(f'loss = {loss}')
+            #    pred = (out > 0.5).float()
+            #    label = data.y.float().reshape(-1, target_dim)
+            #    logger.debug(f'precision = {((label * pred).sum()/label.sum()).item()}')
+            #    logger.debug(f'recall = {((label * pred).sum()/pred.sum()).item()}')
+            #    logger.debug(f'total # of go terms = {label.sum().item()}')
+            #    logger.debug(f'predicted # of go terms = {pred.sum().item()}')
+            #    logger.debug('')
                 
-def evaluate(model, dataset_train, dataset_val, target_dim):
+def evaluate(model, dataset, target_dim):
     
     model.eval()
-    train_loader = DataLoader(dataset_train, batch_size=64)
-    val_loader = DataLoader(dataset_val, batch_size = 64)
+    data_loader = DataLoader(dataset, batch_size=64)
     with torch.no_grad():
-        train_logits = {}
-        val_logits = {}
-        train_labels = {}
-        val_labels = {}
-        train_precisions = []
-        train_recalls = []
-        val_precisions = []
-        val_recalls = []
-        for thresh in torch.arange(0.01, .99, .01):
-            train_precision = 0.0
-            train_recall = 0.0
-            train_num_batches = 0.0
-            for idx, data in enumerate(train_loader):
-                data = data.to(device)
-                if idx in train_logits:
-                    pred = (train_logits[idx] > thresh).float()
-                    label = train_labels[idx]
-                else:
-                    logits = model(data)
-                    train_logits[idx] = logits
-                    pred = (logits > thresh).float()
-                    label = data.y.float().reshape(-1, target_dim)
-                    train_labels[idx] = label
-                if label.sum().item() != 0:
-                    train_precision += ((label * pred).sum()/label.sum()).item()
-                if pred.sum().item() != 0:
-                    train_recall += ((label * pred).sum()/pred.sum()).item()
-                train_num_batches += 1
-            #print('precision = ', train_precision/train_num_batches)
-            #print('recall = ', train_recall/train_num_batches)
-            train_precisions.append(train_precision/train_num_batches)
-            train_recalls.append(train_recall/train_num_batches)
-
-            val_precision = 0.0
-            val_recall = 0.0
-            val_num_batches = 0.0
-
-            for data in val_loader:
-                data = data.to(device) 
-                if idx in val_logits:
-                    pred = (val_logits[idx] > thresh).float()
-                    label = val_labels[idx]
-                else:
-                    logits = model(data)
-                    val_logits[idx] = logits
-                    pred = (logits > thresh).float()
-                    label = data.y.float().reshape(-1, target_dim)
-                    val_labels[idx] = label
-                if label.sum().item() != 0:
-                    val_precision += ((label * pred).sum()/label.sum()).item()
-                if pred.sum().item() != 0:
-                    val_recall += ((label * pred).sum()/pred.sum()).item()
-                val_num_batches += 1
-            #print('precision = ', val_precision/val_num_batches)
-            #print('recall = ', val_recall/val_num_batches)
-            val_precisions.append(val_precision/val_num_batches)
-            val_recalls.append(val_recall/val_num_batches)
+        
+        logits = None
+        labels = None
+        
+        for data in data_loader:
+            data = data.to(device) 
+            if logits is None:
+                logits = model(data).float().cpu().numpy()
+                labels = data.y.float().reshape(-1, target_dim).cpu().numpy()
+            else:
+                logits = np.vstack((logits, model(data).float().cpu().numpy()))
+                labels = np.vstack((labels, data.y.float().reshape(-1, target_dim).cpu().numpy()))
             
-    return train_precisions, train_recalls, val_precisions, val_recalls
+    return logits, labels
+
+def compute_pr(scores, labels, target_dim):
+    
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+    for i in range(target_dim):
+        precision[i], recall[i], _ = precision_recall_curve(labels[:, i], scores[:, i])
+        average_precision[i] = average_precision_score(labels[:, i], scores[:, i])
+
+    # A "micro-average": quantifying score on all classes jointly
+    precision["micro"], recall["micro"], _ = precision_recall_curve(labels.ravel(), scores.ravel())
+    average_precision["micro"] = round(average_precision_score(labels, scores, average="micro"), 3)
+    
+    return precision["micro"], recall["micro"], average_precision["micro"]
+        
+    
         
 def main():
     
@@ -192,7 +146,7 @@ def main():
     
     data_dir = conf['path']['gcn_data'] + '/data.pkl'
     if not os.path.exists(data_dir):
-        data.main()
+        pdb_data.main()
     try:
         with open(data_dir, 'rb') as f:
             data_dict = pickle.load(f)
@@ -208,40 +162,59 @@ def main():
     logger.debug(f'size of training set = {len(train_data)}')
     logger.debug(f'size of validation set = {len(val_data)}')
     logger.debug(f'# of go terms = {target_dim}')
+        
+    plt.figure(figsize = (10, 6))
     
-    if conf['path']['model'] is not None and conf['path']['model'] != '':
+    lm_model_paths = conf['path']['lm']
+    
+    if type(lm_model_paths) != list:
+        lm_model_paths = [lm_model_paths]
+    
+    for lm_model_path in lm_model_paths:
         
-        model = torch.load(conf['path']['model'])
-        
-    else:
-        
-        lm_model_path = conf['path']['lm']
         language_model = torch.load(lm_model_path, map_location=device)
         lm_embedding = language_model['model_state_dict']['module.word_embeddings.weight']
-        
-        model = GCN(target_dim, lm_embedding)
-        
-    model.to(device)
-    
-    train(logger, model, train_data, val_data, target_dim)
-    
-    train_precisions, train_recalls, val_precisions, val_recalls = evaluate(model, train_data, val_data, target_dim)
-    
-    plt.plot(train_precisions, train_recalls)
-    plt.plot(val_precisions, val_recalls)
-    plt.title('Averaged Precision Recall for Molecular Function')
-    plt.legend([f'train auc = {round(auc(train_precisions, train_recalls), 3)}', f'val auc = {round(auc(val_precisions, val_recalls), 3)}'])
+                        
+        num_trials = int(conf['model']['num_trials'])
+        for i in range(num_trials):
+            
+            model_name = lm_model_path.split('/')[-1].split('.')[0] + '_GCN' + '_iter_{}'.format(i)
+            
+            if conf['model']['model'] == 'GCN':
+
+                model = GCN(target_dim, lm_embedding)
+            
+            elif conf['model']['model'] == 'Toy':
+                
+                 model = Toy(target_dim, lm_embedding)
+                
+
+            model.to(device)
+            
+            logger.debug(f'traning on {model_name}')
+
+            train(logger, model, train_data, val_data, target_dim)
+            
+            model_dir = conf['path']['models']
+            torch.save(model, model_dir + '/' + model_name + '.model')
+
+            scores, labels = evaluate(model, val_data, target_dim)
+            
+            precision, recall, ap_score = compute_pr(scores, labels, target_dim)
+            
+            logger.debug('Average precision score for {}: {}'.format(model_name, ap_score))
+            
+            plt.step(recall, precision, where='post', label = 'AP score for {}: {}'.format(model_name, ap_score))
+            
     plt.xlabel('Recall')
     plt.ylabel('Precision')
-    plt.savefig('log/temp.png')
-    
-    if conf['path']['model'] is not None and conf['path']['model'] != '':
-        
-        torch.save(model, conf['path']['model'])
-        
-    else:
-        
-        torch.save(model, 'gcn.temp')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('micro-averaged PR curve over all classes')
+    plt.legend()
+    figure_dir = conf['path']['figures']
+    plt.savefig(figure_dir + '/temp.png')   
+    logger.debug('plot saved to: ' + figure_dir + '/temp.png')
 
 if __name__ == '__main__':
     
